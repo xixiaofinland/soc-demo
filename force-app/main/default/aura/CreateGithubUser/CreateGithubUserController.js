9a6377aaff6c5ff5e34160623919146f3bdb3690
Xi Xiao <xi.xiao@biit.fi>
2018-12-24 16:26:45 +0200

    +

--- force-app/main/default/aura/CreateGithubUser/CreateGithubUserController.js
+++ force-app/main/default/aura/CreateGithubUser/CreateGithubUserController.js
@@ -1,5 +1,38 @@
 ({
-    myAction : function(component, event, helper) {
+  doCreateAction: function (cmp, event, helper) {
+    console.log("in doCreateAction");
 
-    }
+    var spinner = cmp.find("spinner");
+    $A.util.removeClass(spinner, "slds-hide");
+
+    var action = cmp.get("c.createNewUser");
+    var userName = cmp.get("v.userName");
+    console.log('userName: ' + userName);
+
+    action.setParams({ "newUserName" : userName });
+    action.setCallback(this, function(response) {
+      console.log("callback here.");
+      var state = response.getState();
+      if (state === "SUCCESS") {
+        $A.get('e.force:refreshView').fire();
+        console.log(response.getReturnValue());
+      } else if(state === "ERROR") {
+        let errors = response.getError();
+        let message = 'Unknown error';
+        if (errors && Array.isArray(errors) && errors.length > 0) {
+            message = errors[0].message;
+        }
+        console.error(message);
+      }
+      $A.util.addClass(spinner, "slds-hide");
+      $A.get("e.force:closeQuickAction").fire();
+      cmp.destroy();
+    });
+    $A.enqueueAction(action);
+  },
+
+  doCancelAction : function(cmp, event, helper) {
+    $A.get("e.force:closeQuickAction").fire();
+    cmp.destroy();
+  }
 })
--- force-app/main/default/aura/UpdateGithubUser/UpdateGithubUserController.js
+++ force-app/main/default/aura/UpdateGithubUser/UpdateGithubUserController.js
@@ -1,5 +1,29 @@
 ({
-    myAction : function(component, event, helper) {
+  doInit: function (cmp, event, helper) {
+    console.log("in doInit");
 
-    }
+    var spinner = cmp.find("spinner");
+    $A.util.removeClass(spinner, "slds-hide");
+
+    var action = cmp.get("c.updateGithubUser");
+
+    action.setParams({ "githuUserIdAsString": cmp.get("v.recordId") });
+    action.setCallback(this, function(response) {
+      console.log("callback here.");
+      var state = response.getState();
+      if (state === "SUCCESS") {
+        $A.get('e.force:refreshView').fire();
+      } else if(state === "ERROR") {
+        let errors = response.getError();
+        let message = 'Unknown error';
+        if (errors && Array.isArray(errors) && errors.length > 0) {
+            message = errors[0].message;
+        }
+        console.error(message);
+      }
+      $A.util.addClass(spinner, "slds-hide");
+      $A.get("e.force:closeQuickAction").fire();
+    });
+    $A.enqueueAction(action);
+  }
 })
